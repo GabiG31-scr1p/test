eebd14e7790d0fbb220d8e129cc278434f6e6b62
GabiG31 <78113762+GabiG31-scr1p@users.noreply.github.com>
2025-06-15 16:42:12 +0300

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -19,7 +19,19 @@ function Write-Unsuccess {
   [CmdletBinding()]
   param ()
   process {
-    Write-Host -Object ' > ERROR' -ForegroundColor 'Green'
+    Write-Host -Object ' > ERROR' -ForegroundColor 'Red'
+  }
+}
+
+function Test-Admin {
+  [CmdletBinding()]
+  param ()
+  begin {
+    Write-Host -Object "Checking if the script is not being run as administrator..." -NoNewline
+  }
+  process {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    -not $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
   }
 }
 
@@ -66,7 +78,7 @@ function Get-Spicetify {
         $targetVersion = $v
       }
       else {
-        Write-Warning -Message "You have spicefied an invalid spicetify version: $v `nThe version must be in the following format: 1.2.3"
+        Write-Warning -Message "You have specified an invalid spicetify version: $v `nThe version must be in the following format: 1.2.3"
         Pause
         exit
       }
@@ -138,35 +150,9 @@ function Install-Spicetify {
 #region Main
 #region Checks
 if (-not (Test-PowerShellVersion)) {
-  Write-Unsuccess
-  Write-Warning -Message 'PowerShell 5.1 or higher is required to run this script'
-  Write-Warning -Message "You are running PowerShell $($PSVersionTable.PSVersion)"
-  Write-Host -Object 'PowerShell 5.1 install guide:'
-  Write-Host -Object 'https://learn.microsoft.com/skypeforbusiness/set-up-your-computer-for-windows-powershell/download-and-install-windows-powershell-5-1'
-  Write-Host -Object 'PowerShell 7 install guide:'
-  Write-Host -Object 'https://learn.microsoft.com/powershell/scripting/install/installing-powershell-on-windows'
-  Pause
-  exit
-}
-else {
   Write-Success
 }
 if (-not (Test-Admin)) {
-  Write-Unsuccess
-  Write-Warning -Message "The script was run as administrator. This can result in problems with the installation process or unexpected behavior. Do not continue if you do not know what you are doing."
-  $Host.UI.RawUI.Flushinputbuffer()
-  $choices = [System.Management.Automation.Host.ChoiceDescription[]] @(
-    (New-Object System.Management.Automation.Host.ChoiceDescription '&Yes', 'Abort installation.'),
-    (New-Object System.Management.Automation.Host.ChoiceDescription '&No', 'Resume installation.')
-  )
-  $choice = $Host.UI.PromptForChoice('', 'Do you want to abort the installation process?', $choices, 0)
-  if ($choice -eq 0) {
-    Write-Host -Object 'spicetify installation aborted' -ForegroundColor 'Yellow'
-    Pause
-    exit
-  }
-}
-else {
   Write-Success
 }
 #endregion Checks
@@ -178,10 +164,7 @@ Write-Host -Object "`nRun" -NoNewline
 Write-Host -Object ' spicetify -h ' -NoNewline -ForegroundColor 'Cyan'
 Write-Host -Object 'to get started'
 #endregion Spicetify
-  Write-Host -Object 'Starting the spicetify Marketplace installation script..'
-  $Parameters = @{
-    Uri             = 'https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1'
-    UseBasicParsing = $true
-  }
+
   Invoke-WebRequest @Parameters | Invoke-Expression
+
 #endregion Main
